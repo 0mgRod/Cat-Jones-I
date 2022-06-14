c94e45297425d9d297e2656c105e695e447eea16
0mgRod <89850217+0mgRod@users.noreply.github.com>
2022-06-14 22:28:47 +0100

    Update game.py

--- game.py
+++ game.py
@@ -1,52 +1,22 @@
-
-# import the pygame module 
-
-import pygame 
-
-  
-# Define the background colour 
-# using RGB color coding. 
-
-background_colour = (255, 255, 255) 
-
-  
-# Define the dimensions of 
-# screen object(width,height) 
-
-screen = pygame.display.set_mode((300, 300)) 
-
-  
-# Set the caption of the screen 
-
-pygame.display.set_caption('Geeksforgeeks') 
-
-  
-# Fill the background colour to the screen 
-screen.fill(background_colour) 
-
-  
-# Update the display using flip 
-pygame.display.flip() 
-
-  
-# Variable to keep our game loop running 
-
-running = True
-
-  
-# game loop 
-
-while running: 
-
-    
-# for loop through the event queue   
-
-    for event in pygame.event.get(): 
-
-      
-
-        # Check for QUIT event       
-
-        if event.type == pygame.QUIT: 
-
-            running = False
+import pygame, sys
+ 
+clock = pygame.time.Clock()
+ 
+from pygame.locals import *
+pygame.init() # initiates pygame
+ 
+pygame.display.set_caption('My Pygame Window')
+ 
+WINDOW_SIZE = (400,400)
+ 
+screen = pygame.display.set_mode(WINDOW_SIZE,0,32) # initiate the window
+ 
+while True: # game loop
+ 
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+ 
+    pygame.display.update()
+    clock.tick(60)
